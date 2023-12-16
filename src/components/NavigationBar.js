a0f17818d09ef35e784b3ce1d94f3e0845ff173e
Boniiek <boniiek25@gmail.com>
2023-12-16 16:29:06 +0300

    addProfileWindow

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -9,13 +9,21 @@ import Coin from "../images/coin.png"
 const NavigationBar =()=>{
     const {user} = useContext(Context)
     const [modalActive,setModalActive] = useState(false)
+    const [profileActive,setProfileActive] = useState(false)
+    const [balance,SetBalance]=useState(115)
+    const [newBalance,SetNewBalance]=useState(0)
+
+    const logOut =()=>{
+        setProfileActive(false)
+        user.setIsAuth(false)
+    }
 
    return(
     <div className="Navbar">
         <div style={{padding:"50px"}}>
             <span className="firstPartOfName" style={{fontSize:"35px"}}>4S</span><span className="secondPartOfName" style={{fontSize:"35px"}}>tudents</span>
         </div>
-        {user.isAuth  ?
+        {!user.isAuth  ?
         <div style={{padding:"20px"}}>
             <button className="ButtonNavbar"> Вход</button>
             <button className="ButtonNavbar"> Регистрация</button>
@@ -43,7 +51,42 @@ const NavigationBar =()=>{
                     </button>
                 </div>
             </Modal>
-            <button className="ProfileBtn" ><img className="ProfileIcon" alt="img" src={ProfileIcon}/></button>
+            <button className="ProfileBtn" onClick={()=>setProfileActive(true)}><img className="ProfileIcon" alt="img" src={ProfileIcon}/></button>
+            <div className={profileActive ? "modalProfile active" : "modalProfile"} onClick={()=>setProfileActive(false)}>
+                <div className={profileActive ? "modalContentProfile active" : "modalContentProfile"} onClick={e=>e.stopPropagation()}>
+                   <div>
+                        <div style={{display:"flex",justifyContent:"space-between"}}>
+                            <div className="nameProfileModal">@mafioznik</div>
+                            <button style={{}} className="ProfileBtnModal" onClick={()=>setProfileActive(false)}><img className="ProfileIconModal" alt="img" src={ProfileIcon}/></button>
+                        </div>
+                        <div className="emailProfileModal">
+                            Почта:
+                        </div>
+                        <div className="emailModal">
+                            zubenko_m_p@mail.ru
+                        </div>
+                        <div className="emailProfileModal" style={{paddingTop:"10px",display:"flex"}}>
+                            Баланс:
+                            <div className="balanceProfileModal">
+                                {balance} <img alt="icon" src={Coin} style={{height:"23px",}}></img>
+                            </div>
+                        </div>
+                        <div style={{paddingLeft:"10px",paddingTop:"10px",display:"flex",padding:"10px"}}>
+                            
+                            <button className="balanceBtnModal" onClick={()=>SetBalance(+balance+ +newBalance)}>Пополнить</button>
+                            <input className="balanceInputModal" value={newBalance} onChange={(e)=>SetNewBalance(e.target.value)} ></input>
+                        </div>
+                        <div style={{paddingLeft:"10px",paddingTop:"10px",display:"flex",padding:"10px"}}>
+                            
+                            <button className="balanceBtnModal" onClick={()=>SetBalance(+balance - +newBalance)}>Вывести</button>
+                            <input className="balanceInputModal" value={newBalance} onChange={(e)=>SetNewBalance(e.target.value)} ></input>
+                        </div>
+                        <div style={{padding:"0 10px", width:"auto"}}>
+                            <button className="logoutBtnProfileModal" onClick={()=>logOut()} >Выйти</button>
+                        </div>
+                   </div>
+                </div>
+            </div>
             
         </div>  
          }
